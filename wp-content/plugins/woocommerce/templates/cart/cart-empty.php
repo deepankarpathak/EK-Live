6137b6c700df8e48aadf15c7db5671a9cb1aad63
root <gambheer.singh@aurigait.com>
2015-08-21 02:04:27 +0530

    Cart + checkout

--- wp-content/plugins/woocommerce/templates/cart/cart-empty.php
+++ wp-content/plugins/woocommerce/templates/cart/cart-empty.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-wc_print_notices();
+//wc_print_notices();
 
 ?>
 
@@ -20,3 +20,6 @@ wc_print_notices();
 <?php do_action( 'woocommerce_cart_is_empty' ); ?>
 
 <p class="return-to-shop"><a class="button wc-backward" href="<?php echo apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ); ?>"><?php _e( 'Return To Shop', 'woocommerce' ) ?></a></p>
+<script>
+	$(".message-success a").hide();
+</script>
