9b3f02f6c4eb93421e6c041369e66a0a0e44cf4d
root <sachin.ojha@aurigait.com>
2015-09-10 11:18:08 +0530

    Search page 3 column layout and design

--- wp-content/plugins/algoliasearch-wordpress-master/themes/woo/templates.php
+++ wp-content/plugins/algoliasearch-wordpress-master/themes/woo/templates.php
@@ -15,7 +15,7 @@
 </script>
 
 <script type="text/template" id="instant-content-template">
-    <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12  hits{{#facets_count}} with_facets{{/facets_count}}">
+    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12 min-pad hits{{#facets_count}} with_facets{{/facets_count}}">
         {{#hits.length}}
         <div class="infos">
             <div id="result_for">
@@ -39,12 +39,8 @@
                 </select>
             </div>
             {{/sorting_indices.length}}
-			<div class="button-123 right hidden-xs">
-                <button class="grid changelook" onclick=" $('#view li').removeClass('list').addClass('grid'); $('#view .list-images').removeClass('list-images').addClass('grid-images'); $('#view .result-sub-content-list').removeClass('result-sub-content-list').addClass('result-sub-content-grid'); $('#view .price-list').removeClass('price-list').addClass('price-grid');" ><img src="<?php echo get_site_url(); ?>/images/grid_view.png" height="16px" width="16px"/></button>
-                <button class="list changelook" onclick="$('#view li').removeClass('grid').addClass('list'); $('#view .grid-images').removeClass('grid-images').addClass('list-images'); $('#view .result-sub-content-grid').removeClass('result-sub-content-grid').addClass('result-sub-content-list'); $('#view .price-grid').removeClass('price-grid').addClass('price-list');"><img src="<?php echo get_site_url(); ?>/images/list_view.png" height="16px" width="16px"/></button>
-            </div>
             <div style="clear: both;"></div>
-            <div class="labels left"></div>
+            <div class="labels"></div>
         </div>
         
         {{/hits.length}}
@@ -72,12 +68,15 @@
                                     	{{{ _highlightResult.title.value }}} - {{pa_specialization}}
                                 	</h4>
                                     <div class="institute">{{university}}</div>
+                                    <div class="mode-duration-wrapper">
                                     <div class="mode"> Study Content: {{pa_study-content}}</div>
 									<div class="duration">{{pa_duration}}</div>
-                                    <div class="price-grid"> ₹ {{_price}}</div>
+                                    <div class="price-coupan-wrapper">
 									{{#pa_referral-cashback.length}}
-									<div class="referral"><span class="cashback">Cashback</span> ₹ {{pa_referral-cashback}}</div>
+									<div class="referral"><span class="cashback">Cashback</span> Rs. {{pa_referral-cashback}}</div>
 									{{/pa_referral-cashback.length}}
+                                    <div class="price-grid"> Rs. {{_price}}</div>
+                                    </div>
                                 </div>
                             </div>
                             <div class="quick_view_overlay">
@@ -95,9 +94,21 @@
         
         <div style="clear: both;"></div>
     </div>
+    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 min-pad right-pad-none">
+        <div class="button-123 hidden-xs">
+            <select>
+                <option>Popularity</option>
+            </select>
+            <button class="list changelook" onclick="$('#view li').removeClass('grid').addClass('list'); $('#view .grid-images').removeClass('grid-images').addClass('list-images'); $('#view .result-sub-content-grid').removeClass('result-sub-content-grid').addClass('result-sub-content-list'); $('#view .price-grid').removeClass('price-grid').addClass('price-list');"><img src="<?php echo get_site_url(); ?>/images/list.png"/></button>
+            <button class="grid changelook" onclick=" $('#view li').removeClass('list').addClass('grid'); $('#view .list-images').removeClass('list-images').addClass('grid-images'); $('#view .result-sub-content-list').removeClass('result-sub-content-list').addClass('result-sub-content-grid'); $('#view .price-list').removeClass('price-list').addClass('price-grid');" ><img src="<?php echo get_site_url(); ?>/images/grid.png"/></button>
+        </div>
+        <img src="http://localhost/edukart/wp-content/uploads/ad1.jpg" alt="advertise">
+        <img src="http://localhost/edukart/wp-content/uploads/ad2.jpg" alt="advertise">
+    </div>
+
 {{^hits.length}}
 		<div class="row">
-        	<div class="col-lg-12 col-md-12">Sorry, You’re looking for <strong>{{query}}</strong> which isn’t here. However, we have wide range of courses which will help you enhance your skills.<br/><br/>For Certificates, <a href="http://edukart.com/edukart/#q=certificate&page=0&refinements=%5B%5D&numerics_refinements=%7B%7D&index_name=%22tryall%22
+        	<div class="col-lg-12 col-md-12 ">Sorry, You’re looking for <strong>{{query}}</strong> which isn’t here. However, we have wide range of courses which will help you enhance your skills.<br/><br/>For Certificates, <a href="http://edukart.com/edukart/#q=certificate&page=0&refinements=%5B%5D&numerics_refinements=%7B%7D&index_name=%22tryall%22
 ">Click here</a>  |   For Entrance Coaching, <a href="http://edukart.com/edukart/#q=entrance&page=0&refinements=%5B%5D&numerics_refinements=%7B%7D&index_name=%22tryall%22">Click here.</a>  |  For School Education, <a href="http://edukart.com/edukart/#q=class&page=0&refinements=%5B%5D&numerics_refinements=%7B%7D&index_name=%22tryall%22">Click here</a>   |   For Degree Programs, <a href="http://edukart.com/edukart/#q=degree&page=0&refinements=%5B%5D&numerics_refinements=%7B%7D&index_name=%22tryall%22">Click here</a>  |   For Diplomas, <a href="http://edukart.com/edukart/#q=diploma&page=0&refinements=%5B%5D&numerics_refinements=%7B%7D&index_name=%22tryall%22">Click here</a><br/>
         	</div>
 		</div>
@@ -105,10 +116,10 @@
 </script>
 
 <script type="text/template" id="instant-facets-template">
-<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 facets{{#count}} with_facets{{/count}}">
+<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 left-pad-none min-pad facets{{#count}} with_facets{{/count}}">
     <div class="clear_all_div">
-       <button class="clear_all" onclick="clear_all()">Clear All</button>
-    </div>
+       <button class="clear_all" onclick="clear_all()"><span class="refresh sprite"></span><span class="clear-btn">Clear All</span></button>
+    </div> 
     
     {{#facets}}
     {{#count}}
@@ -174,6 +185,7 @@
     </div>
 </div>
 </script>
+
 
 <script type="text/template" id="instant-pagination-template">
 <div class="pagination-wrapper{{#facets_count}} with_facets{{/facets_count}}">
--- wp-content/plugins/algoliasearch-wordpress-master/themes/woo/theme.js
+++ wp-content/plugins/algoliasearch-wordpress-master/themes/woo/theme.js
@@ -136,7 +136,7 @@ jQuery(document).ready(function ($) {
         {
             var html_content = "";
 
-            html_content += "<div id='algolia_instant_selector'><div class='univ_logo_outer  clearfix' style='background:#ccc;display:none'><div class='university_logo_desc row'></div></div><div class='row'><div class='banner_img_container row' style='text-align:center;'></div>";
+            html_content += "<div id='algolia_instant_selector'><div class='univ_logo_outer  clearfix' style='background:#ccc;display:none'><div class='university_logo_desc row'></div></div><div class='row'><div class='banner_img_container' style='text-align:center;'></div>";
 
             var facets = [];
             var pages = [];
@@ -171,7 +171,7 @@ jQuery(document).ready(function ($) {
                 var data_tax = $(this).attr("data-tax");
                 var raw_label_html = $(".raw_labels").html();
                 if($(".raw_labels").html().indexOf(data_name)<=0){
-                    $(".raw_labels").html(raw_label_html+"<div class='label' data-tax='"+data_tax+"' data-name='"+data_name+"'>"+data_name+"<span class='close_label'>X</span></div>");
+                    $(".raw_labels").html(raw_label_html+"<div class='label' data-tax='"+data_tax+"' data-name='"+data_name+"'>"+data_name+"<span class='close_label'>x</span></div>");
                     }
                }
             });
