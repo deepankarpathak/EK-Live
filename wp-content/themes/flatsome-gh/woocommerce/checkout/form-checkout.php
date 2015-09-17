1eb6ae1b89a5ea009609deb9c4e7cffeb1e0a8d8
Somesh <somesh.soni@aurigait.com>
2015-09-17 19:15:32 +0530

    Checkout email validation issue fix

--- wp-content/themes/flatsome-gh/woocommerce/checkout/form-checkout.php
+++ wp-content/themes/flatsome-gh/woocommerce/checkout/form-checkout.php
@@ -18,22 +18,14 @@ wc_print_notices();
 <script type="text/javascript">
 
     function validateemail(emailadd) {
-        var index_at = emailadd.indexOf('@')
-        if (index_at == -1) {
-            return false;
-        }
+        
+	console.log(emailadd);
+	var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
 
-        var name = emailadd.substr(0, index_at);
-        /* should test name for other invalids*/
-
-        var domain = emailadd.substr(index_at + 1);
-        /* should check for extra "@" and any other checks that would invalidate an address of which there are likely many*/
-        if (domain.indexOf('@') != -1) {
-            return false;
-        }
-
-        return domain.indexOf('.') > 1;
-
+	if ( ! pattern.test( emailadd) ) {
+		return false;
+	}
+	return true;
     }
 
     jQuery(document).ready(function() {
