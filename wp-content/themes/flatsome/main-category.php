f22db7e1795042073839b09b1bd043426dbbdad6
root <gambheer.singh@aurigait.com>
2015-09-24 17:45:28 +0530

    New Template created

---
+++ wp-content/themes/flatsome/main-category.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template name: Main Category
+*/
+get_header(); ?>
+
+<?php if( has_excerpt() ) { ?>
+<div class="page-header">
+	<?php the_excerpt(); ?>
+</div>
+<?php } ?>
+
+<div  class="page-wrapper">
+<div class="row">
+<div id="content" class="large-12 columns" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php the_content(); ?>
+			
+			<?php endwhile; // end of the loop. ?>
+
+</div><!-- #content -->
+</div><!-- .row  -->
+</div><!-- .page-wrapper -->
+
+<?php get_footer(); ?>
